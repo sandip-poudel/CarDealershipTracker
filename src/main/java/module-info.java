28b99f46484a1bc3b1918cc79f2307db057ea2f4
K.Port <145790983+Zabzar22@users.noreply.github.com>
2025-03-26 15:17:31 -0500

    Create module-info.java
    to remove red error msg on launch

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module DealershipSoftware {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.annotation;
+    requires java.xml;
+    exports org.example;
+}
